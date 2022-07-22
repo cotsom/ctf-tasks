c697cb03839a19630ecff4e96ce41b15db13bb5e
cotsom <gerfy.17@mail.ru>
2022-07-22 17:04:43 +0300

    add checker

--- date-rce/scripts/checkers/checker.py
+++ date-rce/scripts/checkers/checker.py
@@ -8,9 +8,15 @@ def ping():
     r = requests.get(f"http://{host}:{port}/ping", timeout=2)
     if r.status_code == 200:
         if r.text == "pong":
-            print(1)
             return 1
     return 0
 
+def get():
+    r = requests.post(f"http://{host}:{port}", data={"date": "date"})
+    if r.status_code == 200:
+        return 1
+    return 0
+
 if __name__ == '__main__':
-    ping()
+    if (ping() and get()):
+        print(1)
